33bb6f98be0711e3ed0456bbd429e0184ed4bba3
choi232 <148494133+choi232@users.noreply.github.com>
2025-02-05 09:44:27 -0500

    Create Company.java

---
+++ Company.java
@@ -0,0 +1,8 @@
+public class Company{
+    private int companyID;
+    private String companyName;
+    public Company(int setCompanyID, String setCompanyName){
+        companyID = setCompanyID;
+        companyName = setCompanyName;
+    }
+}
