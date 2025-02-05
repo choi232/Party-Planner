5d39ca091864094bf46722b5f9731a02ff3c70ef
choi232 <148494133+choi232@users.noreply.github.com>
2025-02-05 09:44:00 -0500

    Create Party.java

---
+++ Party.java
@@ -0,0 +1,55 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.ArrayList; //Import ArrayList class
+
+
+public class Party{
+    private ArrayList<ArrayList<Attendee>> attendeeList = new ArrayList<ArrayList<Attendee>>();
+    private ArrayList<Company> companyList = new ArrayList<Company>();
+    private int numCompany;
+    private int numSeats;
+    private int numTables;
+
+    public Party(int setNumSeats, int setNumTables){
+        numSeats = setNumSeats;
+        numTables = setNumTables;
+    }
+
+    public void readFile(){
+        try {
+            File companyFile = new File("companies.txt");
+            File partyGuestsFile = new File("partyguests.txt");
+
+            Scanner scanCompany = new Scanner(companyFile);
+            Scanner scanGuests = new Scanner(partyGuestsFile);
+
+            while(scanCompany.hasNextLine()){
+                String line = scanCompany.nextLine();
+                String[] splitStr = line.split(",");
+                System.out.println(Integer.parseInt(splitStr[0]));
+                Company company = new Company(Integer.parseInt(splitStr[0]), splitStr[1]);
+                companyList.add(company);
+            }
+
+            while(scanGuests.hasNextLine()){
+                String line = scanGuests.nextLine();
+            }
+        }
+
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void register(){
+
+    }
+
+    public void printCompanyList(){
+        for(int i = 0; i < numCompany; i++){
+            System.out.println(companyList.get(i));
+        }
+    }
+}
