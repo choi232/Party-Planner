07393bc83e124aabcaaf6e6d380590bacf59e74d
choi232 <148494133+choi232@users.noreply.github.com>
2025-02-11 09:43:53 -0500

    Update Company.java

--- Company.java
+++ Company.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class Company{
     private int companyID;
     private String companyName;
+    private ArrayList<Attendee> employee = new ArrayList<Attendee>();
+    private int numEmployee;
     public Company(int setCompanyID, String setCompanyName){
         companyID = setCompanyID;
         companyName = setCompanyName;
@@ -11,5 +15,20 @@ public class Company{
     }
     public String getCompanyName(){
         return companyName;
+    }
+
+    public int getNumEmployee(){
+        return numEmployee;
+    }
+
+    public Attendee getEmployee(int index){
+        return employee.get(index);
+    }
+
+    public void setEmployee(Attendee attendee, int numTables){
+        if(employee.size() < numTables){
+            employee.add(attendee);
+            numEmployee = employee.size();
+        }
     }
 }
