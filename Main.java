f68b4efbaaaa8418e0f91256791e2bf8e17dadb5
choi232 <148494133+choi232@users.noreply.github.com>
2025-02-21 01:15:05 -0500

    Update Main.java
    comments

--- Main.java
+++ Main.java
@@ -1,13 +1,22 @@
+/** 
+ * Main.java File for creating Party object and running its methods
+ * @author Mikael Choi 
+ * @since 2/20/2025
+ * Preconditions: Party Class/Object
+ * Postconditions: runs entire Party Planner program through instance from main method
+ * Purpose: Consolidate all the code being ran into one spot, particularly in the main method.
+ * Makes it extremely easy to just create an instance of party and run the entire program from Main
+ **/
 
+/*
+Main Class: Tester to run Party
+*/
 public class Main{
+    //Main Method where all the magic happens
     public static void main(String[] args){
-        // ArrayList<Attendee> attList = new ArrayList<Attendee>();
-        // Attendee mrTwyford = new Attendee("Mr. Twyford", "Seven Hills", 7);
-        // attList.add(mrTwyford);
-        // System.out.println(attList.get(0).toString());
+        //Create instance of party with args of numSeats = 10 and numTables = 10, respectively
         Party p = new Party(10, 10);
-        p.readFile();
-        p.register();
-        p.printCompanyList();
+        //run instance method to run ENTIRE Party Planner program
+        p.runPartyPlanner();
     }
 }
